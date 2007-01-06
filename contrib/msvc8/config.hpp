e8540c8f11258ae41c7fe5539b38c243596cafcf
Philipp Kern <phil@0x539.de>
2007-01-06 19:44:13 +0000

    [project @ Added MSVC8 project & fake headers.]
    Original author: Michael Walter <michael.walter@gmail.com>
    Date: 2005-09-22 16:10:27+00:00

---
+++ contrib/msvc8/config.hpp
@@ -0,0 +1,29 @@
+/* libobby - Network text editing library
+ * Copyright (C) 2005 0x539 dev group
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef _LIBOBBY_CONTRIB_MSVC8_CONFIG_HPP_
+#define _LIBOBBY_CONTRIB_MSVC8_CONFIG_HPP_
+
+#ifndef _MSC_VER
+#error this "config.hpp" replacement solely targets MSVC8
+#endif
+
+#define PACKAGE_VERSION "0.2.0 (msvc)"
+#define WITH_HOWL 1
+
+#endif
---
+++ contrib/msvc8/inttypes.h
@@ -0,0 +1,37 @@
+/* libobby - Network text editing library
+ * Copyright (C) 2005 0x539 dev group
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef _LIBOBBY_CONTRIB_MSVC8_INTTYPES_H_
+#define _LIBOBBY_CONTRIB_MSVC8_INTTYPES_H_
+
+#ifndef _MSC_VER
+#error this <inttypes.h> replacement solely targets MSVC8
+#endif
+
+/** define various fixed-size integer types which will be available
+ * in an upcoming C++ standard.
+ */
+typedef unsigned __int8 uint8_t;
+typedef unsigned __int16 uint16_t;
+#ifndef HOWL_API
+typedef unsigned __int32 uint32_t;
+#endif
+
+typedef uint32_t int_least16_t;
+
+#endif
