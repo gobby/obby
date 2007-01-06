4bc9939c69af660585f53fb6949d1443e70afb03
Philipp Kern <phil@0x539.de>
2007-01-06 19:39:06 +0000

    [project @ Use the right generated key as the public one]
    Original author: Philipp Kern <phil@0x539.de>
    Date: 2005-07-20 19:55:34+00:00

--- src/server_buffer.cpp
+++ src/server_buffer.cpp
@@ -30,7 +30,7 @@ obby::server_buffer::server_buffer()
 {
 	// TODO: Key length as ctor parameter
 	std::pair<RSA::Key, RSA::Key> keys = RSA::generate(m_rclass, 256);
-	m_public = keys.second; m_private = keys.first;
+	m_public = keys.first; m_private = keys.second;
 }
 
 obby::server_buffer::server_buffer(const RSA::Key& public_key,
