b1d13ffb12775503792bb553e574426f603f06ec
Philipp Kern <phil@0x539.de>
2007-01-06 19:51:01 +0000

    [project @ Use IPv4 instead of IPv6 in host_buffer]
    Original author: Armin Burgmeier <armin@0x539.de>
    Date: 2006-02-04 16:09:29+00:00

--- inc/host_buffer.hpp
+++ inc/host_buffer.hpp
@@ -335,7 +335,7 @@ template<typename Document, typename Selector>
 typename basic_host_buffer<Document, Selector>::base_net_type*
 basic_host_buffer<Document, Selector>::new_net()
 {
-	return new net_type(m_username);
+	return new net_type(m_username, false);
 }
 
 template<typename Document, typename Selector>
