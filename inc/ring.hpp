e02c06e7695f38750880f5d1666c365427c4d886
Philipp Kern <phil@0x539.de>
2007-01-06 19:48:01 +0000

    [project @ Reimplemented ring class using std::list]
    Original author: Armin Burgmeier <armin@0x539.de>
    Date: 2005-11-04 10:46:41+00:00

--- inc/ring.hpp
+++ inc/ring.hpp
@@ -19,7 +19,7 @@
 #ifndef _OBBY_RING_HPP_
 #define _OBBY_RING_HPP_
 
-#include <vector>
+#include <list>
 #include <stdexcept>
 
 namespace obby
@@ -33,67 +33,10 @@ template<typename value_type>
 class ring
 {
 public:
-	typedef std::size_t size_type;
-	typedef std::vector<value_type> container_type;
-
-	template<typename ring_type, typename base_iterator>
-	class basic_iterator
-	{
-	public:
-		basic_iterator(ring_type& container,
-		               const base_iterator& base_iter);
-
-		basic_iterator& operator++();
-		basic_iterator operator++(int);
-
-		basic_iterator& operator--();
-		basic_iterator operator--(int);
-
-		bool operator==(const base_iterator& rhs) const;
-		bool operator!=(const base_iterator& lhs) const;
-
-		bool operator==(const basic_iterator& rhs) const;
-		bool operator!=(const basic_iterator& lhs) const;
-	protected:
-		ring_type& m_ring;
-		base_iterator m_iter;
-	};
-
-	class const_iterator:
-		public basic_iterator<
-			const ring<value_type>,
-			typename container_type::const_iterator
-		>
-	{
-	public:
-		typedef const ring<value_type> ring_type;
-		typedef typename container_type::const_iterator base_iterator;
-
-		const_iterator(ring_type& container,
-		               const base_iterator& base_iter);
-
-		const value_type& operator*() const;
-		const value_type* operator->() const;
-	};
-
-	class iterator:
-		public basic_iterator<
-			ring<value_type>,
-			typename container_type::iterator
-		>
-	{
-	public:
-		typedef ring<value_type> ring_type;
-		typedef typename container_type::iterator base_iterator;
-
-		iterator(ring_type& container,
-		         const base_iterator& base_iter);
-
-		value_type& operator*();
-		value_type* operator->();
-
-		operator const_iterator() const;
-	};
+	typedef std::list<value_type> container_type;
+	typedef typename container_type::size_type size_type;
+	typedef typename container_type::iterator iterator;
+	typedef typename container_type::const_iterator const_iterator;
 
 	/** Creates a ring with <em>n</em> elements before wrapping around.
 	 */
@@ -123,6 +66,10 @@ public:
 	 */
 	bool empty() const;
 
+	/** Clears all contents within the ring.
+	 */
+	void clear();
+
 	/** Returns an STL-like iterator pointing at the first (oldest) element
 	 * in the ring.
 	 */
@@ -145,260 +92,75 @@ public:
 
 private:
 	container_type m_elems;
-	typename container_type::iterator m_begin;
-	typename container_type::iterator m_end;
+	size_type m_n;
 };
 
 template<typename value_type>
-template<typename ring_type, typename base_iterator>
-ring<value_type>::basic_iterator<ring_type, base_iterator>::
-	basic_iterator(ring_type& container,
-	               const base_iterator& base_iter):
-	m_ring(container), m_iter(base_iter)
-{
-}
-
-template<typename value_type>
-template<typename ring_type, typename base_iterator>
-ring<value_type>::basic_iterator<ring_type, base_iterator>&
-ring<value_type>::basic_iterator<ring_type, base_iterator>::operator++()
-{
-	if(m_iter == m_ring.m_end)
-	{
-		m_iter = m_ring.m_elems.end();
-	}
-	else
-	{
-		++ m_iter;
-		if(m_iter == m_ring.m_elems.end() )
-			m_iter = m_ring.m_elems.begin();
-	}
-
-	return *this;
-}
-
-template<typename value_type>
-template<typename ring_type, typename base_iterator>
-ring<value_type>::basic_iterator<ring_type, base_iterator>
-ring<value_type>::basic_iterator<ring_type, base_iterator>::operator++(int)
-{
-	const_iterator temp(*this);
-	operator++();
-	return temp;
-}
-
-template<typename value_type>
-template<typename ring_type, typename base_iterator>
-ring<value_type>::basic_iterator<ring_type, base_iterator>&
-ring<value_type>::basic_iterator<ring_type, base_iterator>::operator--()
-{
-	if(m_iter == m_ring.m_elems.end() )
-	{
-		m_iter = m_ring.m_end;
-	}
-	else
-	{
-		if(m_iter == m_ring.m_elems.begin() )
-			m_iter = m_ring.m_elems.end();
-
-		-- m_iter;
-	}
-
-	return *this;
-}
-
-template<typename value_type>
-template<typename ring_type, typename base_iterator>
-ring<value_type>::basic_iterator<ring_type, base_iterator>
-ring<value_type>::basic_iterator<ring_type, base_iterator>::operator--(int)
-{
-	const_iterator temp(*this);
-	operator--();
-	return temp;
-}
-
-template<typename value_type>
-template<typename ring_type, typename base_iterator>
-bool ring<value_type>::basic_iterator<ring_type, base_iterator>::
-	operator==(const base_iterator& rhs) const
-{
-	return m_iter == rhs;
-}
-
-template<typename value_type>
-template<typename ring_type, typename base_iterator>
-bool ring<value_type>::basic_iterator<ring_type, base_iterator>::
-	operator!=(const base_iterator& rhs) const
-{
-	return m_iter != rhs;
-}
-
-template<typename value_type>
-template<typename ring_type, typename base_iterator>
-bool ring<value_type>::basic_iterator<ring_type, base_iterator>::
-	operator==(const basic_iterator& rhs) const
-{
-	return m_iter == rhs.m_iter;
-}
-
-template<typename value_type>
-template<typename ring_type, typename base_iterator>
-bool ring<value_type>::basic_iterator<ring_type, base_iterator>::
-	operator!=(const basic_iterator& rhs) const
-{
-	return m_iter != rhs.m_iter;
-}
-
-template<typename value_type>
-ring<value_type>::const_iterator::
-	const_iterator(ring_type& container,
-	               const base_iterator& base_iter):
-	basic_iterator<ring_type, base_iterator>(container, base_iter)
-{
-}                                                
-
-template<typename value_type>
-const value_type& ring<value_type>::const_iterator::operator*() const
-{
-	return basic_iterator<ring_type, base_iterator>::m_iter.operator*();
-}
-
-template<typename value_type>
-const value_type* ring<value_type>::const_iterator::operator->() const
-{
-	return basic_iterator<ring_type, base_iterator>::m_iter.operator->();
-}
-
-template<typename value_type>
-ring<value_type>::iterator::iterator(ring_type& container,
-                                     const base_iterator& base_iter):
-	basic_iterator<ring_type, base_iterator>(container, base_iter)
-{
-}                             
-
-template<typename value_type>
-value_type& ring<value_type>::iterator::operator*()
-{
-	return basic_iterator<ring_type, base_iterator>::m_iter.operator*();
-}
-
-template<typename value_type>
-value_type* ring<value_type>::iterator::operator->()
-{
-	return basic_iterator<ring_type, base_iterator>::m_iter.operator->();
-}
-
-template<typename value_type>
-ring<value_type>::iterator::
-	operator typename ring<value_type>::const_iterator() const
-{
-	return const_iterator(
-		basic_iterator<ring_type, base_iterator>::m_ring,
-		basic_iterator<ring_type, base_iterator>::m_iter
-	);
-}
-
-template<typename value_type>
 ring<value_type>::ring(size_type n):
-	m_elems(n), m_begin(m_elems.end() ), m_end(m_elems.end() )
+	m_n(n)
 {
 }
 
 template<typename value_type>
 void ring<value_type>::push_back(const value_type& val)
 {
-	if(m_end == m_elems.end() )
-	{
-		// No elements, begin ring with first
-		m_begin = m_end = m_elems.begin();
-	}
-	else
-	{
-		// Advance
-		++ m_end;
-		if(m_end == m_elems.end() ) m_end = m_elems.begin();
-	}
-
-	// Overwrite last
-	if(m_end == m_begin)
-	{
-		++ m_begin;
-		if(m_begin == m_elems.end() )
-			m_begin = m_elems.begin();
-	}
-
-        *m_end = val;
+	m_elems.push_back(val);
+	while(m_elems.size() > m_n)
+		m_elems.pop_front();
 }
 
 template<typename value_type>
 void ring<value_type>::pop_back()
 {
-	// No elements
-	if(m_end == m_elems.end() )
-		throw std::logic_error("obby::ring::pop_back");
-
-	// Removed last element
-	if(m_end == m_begin)
-	{
-		m_begin = m_end = m_elems.end();
-	}
-	else
-	{
-		// Move iterator backwards, the element left out will be
-		// overwritten if it reused
-		if(m_end == m_elems.begin() )
-			m_end = m_elems.end();
-
-		-- m_end;
-	}
+	m_elems.pop_back();
 }
 
 template<typename value_type>
 const value_type& ring<value_type>::front() const
 {
-	if(m_begin == m_elems.end() )
-		throw std::logic_error("obby::ring::front");
-
-	return *m_begin;
+	return m_elems.front();
 }
 
 template<typename value_type>
 const value_type& ring<value_type>::back() const
 {
-	if(m_end == m_elems.end() )
-		throw std::logic_error("obby::ring::back");
-
-	return *m_end;
+	return m_elems.back();
 }
 
 template<typename value_type>
 bool ring<value_type>::empty() const
 {
-	return m_end == m_elems.end();
+	return m_elems.empty();
+}
+
+template<typename value_type>
+void ring<value_type>::clear()
+{
+	m_elems.clear();
 }
 
 template<typename value_type>
 typename ring<value_type>::const_iterator ring<value_type>::begin() const
 {
-	return const_iterator(*this, m_begin); //m_elems.begin() );
+	return m_elems.begin();
 }
 
 template<typename value_type>
 typename ring<value_type>::const_iterator ring<value_type>::end() const
 {
-	return const_iterator(*this, m_end); //elems.end() );
+	return m_elems.end();
 }
 
 template<typename value_type>
 typename ring<value_type>::iterator ring<value_type>::begin()
 {
-	return iterator(*this, m_begin); //elems.begin() );
+	return m_elems.begin();
 }
 
 template<typename value_type>
 typename ring<value_type>::iterator ring<value_type>::end()
 {
-	return iterator(*this, m_end); //elems.end() );
+	return m_elems.end();
 }
 
 } // namespace obby
