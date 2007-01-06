3db1278480822b09bf3eee31d8e062294c38979b
Philipp Kern <phil@0x539.de>
2007-01-06 19:37:42 +0000

    [project @ Preliminary RSA implementation, key generation pending]
    Original author: Philipp Kern <phil@0x539.de>
    Date: 2005-07-18 12:36:52+00:00

---
+++ inc/rsa.hpp
@@ -0,0 +1,71 @@
+/* libobby - Network text editing library
+ * Copyright (C) 2005 0x539 dev group
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef _OBBY_RSA_HPP_
+#define _OBBY_RSA_HPP_
+
+#include <string>
+#include <gmpxx.h>
+
+namespace obby
+{
+
+namespace RSA
+{
+	class Key
+	{
+	public:
+		Key(const mpz_class& n, const mpz_class& k);
+		Key(const Key& other);
+		~Key();
+
+		Key& operator=(const Key& other);
+
+		const std::string& get_id() const;
+		const mpz_class& get_n() const;
+		const mpz_class& get_k() const;
+
+		/** Applies the key components using the RSA algorithm to a
+		 * number to generate a new encrypted one.
+		 */
+		mpz_class apply(const mpz_class& num) const;
+
+	private:
+		mpz_class m_n, m_k;
+		std::string m_id;
+	};
+
+	/** Generates a public/private key pair with a given bit count.
+	 */
+	std::pair<Key, Key> generate(unsigned int bits);
+
+	/** Encrypts a string with a given key to another, binary-safe
+	 * string representation.
+	 */
+	std::string encrypt(const Key& key, const std::string& msg);
+
+	/** Decrypts a string with a given key to the original one,
+	 * if the key matches.
+	 */
+	std::string decrypt(const Key& key, const std::string& msg);
+};
+
+}
+
+#endif // _OBBY_RSA_HPP_
+
---
+++ src/rsa.cpp
@@ -0,0 +1,139 @@
+/* libobby - Network text editing library
+ * Copyright (C) 2005 0x539 dev group
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free
+ * Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "rsa.hpp"
+
+obby::RSA::Key::Key(const mpz_class& n, const mpz_class& k)
+ : m_n(n), m_k(k), m_id((n & mpz_class("ffffffffffffffff", 16)).get_str(16))
+{
+}
+
+obby::RSA::Key::Key(const Key& other)
+ : m_n(other.m_n), m_k(other.m_k), m_id(other.m_id)
+{
+}
+
+obby::RSA::Key::~Key();
+{
+}
+
+obby::RSA::Key& obby::RSA::Key::operator=(const Key& other)
+{
+	m_n = other.m_n;
+	m_k = other.m_k;
+	m_id = other.m_id;
+	return *this;
+}
+
+const std::string& obby::RSA::Key::get_id() const
+{
+	return m_id;
+}
+
+const mpz_class& obby::RSA::Key::get_n() const
+{
+	return m_n;
+}
+
+const mpz_class& obby::RSA::Key::get_k() const
+{
+	return m_k;
+}
+
+mpz_class obby::RSA::Key::apply(const mpz_class& num) const
+{
+	mpz_class t;
+	mpz_powm(t.get_mpz_t(), num.get_mpz_t(), m_k.get_mpz_t(),
+	         m_n.get_mpz_t());
+	return t;
+}
+
+namespace
+{
+	inline void encrypt_num_to_str(const Key& key, const mpz_class& num,
+	                               std::string& res)
+	{
+		res += key.apply(num).get_str(36);
+	}
+
+	inline void decrypt_num_to_str(const Key& key, const mpz_class& num,
+	                               std::string& res)
+	{
+		mpz_class t(key.apply(num) );
+		do
+		{
+			res += (t & 0xff);
+			res >>= 8;
+		} while(t);
+	}
+}
+
+std::pair<Key, Key> obby::RSA::generate(unsigned int bits)
+{
+	/* TODO... IMPLEMENT THIS */
+	/* There is still room for optimisations... */
+}
+
+std::string obby::RSA::encrypt(const Key& key, const std::string& msg)
+{
+	mpz_class t;
+	std::string result;
+	for(std::string::size_type i = 0; i < msg.length(); ++i)
+	{
+		// Perhaps we could optimise here by checking the bit
+		// count of both numbers. [TODO]
+		t <<= 8;
+		t |= msg[i];
+
+		// When the number gets bigger than the RSA modulus, we
+		// need to split it up, otherwise the algorithm fails.
+		if(t >= key.get_n() )
+		{
+			t >>= 8;
+			encrypt_num_to_str(key, t, result);
+			result += '|'; // Separation of the values
+			t = msg[i];
+		}
+	}
+	encrypt_num_to_str(key, t, result);
+	return result;
+}
+
+std::string obby::RSA::decrypt(const Key& key, const std::string& msg)
+{
+	std::string result;
+	std::string::size_type pos = 0, prev = 0;
+
+	// Split at separations
+	while((pos = msg.find('|') ) != std::string::npos)
+	{
+		// Decrypt number between
+		decrypt_num_to_str(
+			key,
+		       	mpz_class(msg.substr(prev, pos - prev), 36),
+		       	result
+		);
+	}
+
+	// Decrypt last part
+	decrypt_num_to_str(key, mpz_class(msg.substr(prev), 36), result);
+	return result;
+}
+
+#endif // _OBBY_RSA_HPP_
+
