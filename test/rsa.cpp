4553bb45c582d6571554d4fe5345eb5dd2cf696d
Philipp Kern <phil@0x539.de>
2007-01-06 19:44:37 +0000

    [project @ Changed RSA test to return non-zero if it fails]
    Original author: Armin Burgmeier <armin@0x539.de>
    Date: 2005-09-29 20:30:20+00:00

--- test/rsa.cpp
+++ test/rsa.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
-#include <cassert>
+#include <stdexcept>
 #include <gmpxx.h>
 #include "rsa.hpp"
 
 using namespace obby;
 
-int main()
+int main() try
 {
 	gmp_randclass rclass(GMP_RAND_ALG_LC, 16);
 	rclass.seed(std::time(NULL) );
 	std::pair<RSA::Key, RSA::Key> my(RSA::generate(rclass, 1024) );
-	assert(my.first.get_n() == my.second.get_n() );
-	std::cout << "n      : " << my.first.get_n() << std::endl;
-	std::cout << "pub    : " << my.first.get_k() << std::endl;
-	std::cout << "priv   : " << my.second.get_k() << std::endl;
-	std::cout << std::endl;
-	std::cout << "apply  : " << my.first.apply(1337) << std::endl;
-	std::cout << "unapply: " << my.second.apply(my.first.apply(1337) ) << std::endl;
-	std::cout << std::endl;
-	std::cout << "encrypt: " << RSA::encrypt(my.first, "foobar") << std::endl;
-	std::cout << "decrypt: " << RSA::decrypt(my.second, RSA::encrypt(my.first, "foobar") ) << std::endl;
-}
 
+	if(my.first.get_n() != my.second.get_n() )
+		throw std::logic_error("key pair modules differ");
+
+	if(my.second.apply(my.first.apply(1337) ) != 1337)
+		throw std::logic_error("key pair does not match");
+
+	if(RSA::decrypt(my.second, RSA::encrypt(my.first, "foobar")) !=
+	   "foobar")
+		throw std::logic_error("en/decryption of text fails");
+
+	std::cout << "RSA test passed" << std::endl;
+	return EXIT_SUCCESS;
+}
+catch(std::exception& e)
+{
+	std::cerr << "RSA test failed: " << e.what() << std::endl;
+	return EXIT_FAILURE;
+}
