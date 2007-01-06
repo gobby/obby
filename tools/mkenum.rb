fe3329fa86dde80256e12034be9caecb5afad46d
Philipp Kern <phil@0x539.de>
2007-01-06 19:43:50 +0000

    [project @ Added mkenum.rb]
    Original author: Armin Burgmeier <armin@0x539.de>
    Date: 2005-09-20 18:09:27+00:00

---
+++ tools/mkenum.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+BASE = <<EOT
+class #NAME#
+{
+public:
+#OPTIONLIST#
+
+        #NAME# operator|(#NAME# other) const { return #NAME#(m_value | other.m_value); }
+        #NAME# operator&(#NAME# other) const { return #NAME#(m_value & other.m_value); }
+        #NAME# operator^(#NAME# other) const { return #NAME#(m_value ^ other.m_value); }
+        #NAME#& operator|=(#NAME# other) { m_value |= other.m_value; return *this; }
+        #NAME#& operator&=(#NAME# other) { m_value &= other.m_value; return *this; }
+        #NAME#& operator^=(#NAME# other) { m_value ^= other.m_value; return *this; }
+        #NAME# operator~() const { return #NAME#(~m_value); }
+
+	operator bool() const { return m_value != NONE; }
+	bool operator!() const { return m_value == NONE; }
+        bool operator==(#NAME# other) { return m_value == other.m_value; }
+        bool operator!=(#NAME# other) { return m_value != other.m_value; }
+
+        unsigned int get_value() const { return m_value; }
+
+protected:
+        explicit #NAME#(unsigned int value) : m_value(value) { }
+
+        unsigned int m_value;
+};
+
+#OPTIONASSIGNMENT#
+EOT
+
+raise Exception, "Not enough parameters specified!" unless ARGV.length > 0
+name = ARGV[0]
+
+optionlist = [['NONE', 0]]; cnt = 1/2.0
+STDIN.each_line { |option| optionlist << [option.strip, (cnt *= 2).floor] }
+
+puts BASE.gsub(/#(\w+)#/) { |token| 
+  case token
+    when '#NAME#': name
+    when '#OPTIONLIST#': optionlist.map { |option|
+        "\tstatic const #{name} #{option[0]};"
+      }.join("\n")
+    when '#OPTIONASSIGNMENT#': optionlist.map { |option|
+        "const %s %s::%s = %s(0x%.8x);" % [name, name, option[0], name, option[1]]
+      }.join("\n")
+  end
+}
+
+# vim:set et ts=2:
